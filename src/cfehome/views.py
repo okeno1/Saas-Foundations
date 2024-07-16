7d2bb274e684123f8fa64a50f6875813be1a10d6
okeno1 <petercoin.1997@gmail.com>
2024-07-17 00:59:03 +0300

    Adding HTML within python code in views.py

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -7,8 +7,21 @@ this_dir = pathlib.Path(__file__).resolve().parent
 def home_page_view(request, *args, **kwargs):
     
     print(this_dir)
-    html_ = ""
-    html_file_path = this_dir/"home.html"
-    html_ = html_file_path.read_text()
+    html_ = """
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <h1>Is this anything? (Inline HTML) </h1>
+</body>
+</html>
+    """
+   # html_file_path = this_dir/"home.html"
+   # html_ = html_file_path.read_text()
         
     return HttpResponse(html_)
