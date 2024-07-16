649d3ca7394eba6f54146ea5300ff5c2ddea7cac
okeno1 <petercoin.1997@gmail.com>
2024-07-17 01:58:18 +0300

    templates

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -1,10 +1,23 @@
 import pathlib
 
+from django.shortcuts import render
 from django.http import HttpResponse
 
 this_dir = pathlib.Path(__file__).resolve().parent
 
 def home_page_view(request, *args, **kwargs):
+    
+    my_title = "My page"
+    my_context={
+        "page_title":my_title
+    }
+    
+    html_template = "home.html"
+        
+    return render(request,html_template,my_context)
+
+
+def my_old_home_page_view(request, *args, **kwargs):
     
     my_title = "My page"
     my_context={
