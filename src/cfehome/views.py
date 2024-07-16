2c4c86ae794a15d7c02a31a85cfc70df87732c24
okeno1 <petercoin.1997@gmail.com>
2024-07-17 00:49:51 +0300

    Initial set up

---
+++ src/cfehome/views.py
@@ -0,0 +1,14 @@
+import pathlib
+
+from django.http import HttpResponse
+
+this_dir = pathlib.Path(__file__).resolve().parent
+
+def home_page_view(request, *args, **kwargs):
+    
+    print(this_dir)
+    html_ = ""
+    html_file_path = this_dir/"home.html"
+    html_ = html_file_path.read_text()
+        
+    return HttpResponse(html_)
