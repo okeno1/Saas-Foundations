203bb73952d0d4266068b8584806d3c33a46097e
okeno1 <petercoin.1997@gmail.com>
2024-07-17 04:20:20 +0300

    object create: all, create, filter, for Sending and retrieving data with a db

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -3,16 +3,29 @@ import pathlib
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from visits.models import PageVisits
+
 this_dir = pathlib.Path(__file__).resolve().parent
 
 def home_page_view(request, *args, **kwargs):
     
+    #query_set = PageVisits.objects.all()
+    page_qs = PageVisits.objects.filter(path=request.path)
+    qs = PageVisits.objects.all() 
     my_title = "My page"
     my_context={
-        "page_title":my_title
+        "page_title": my_title,
+        "page_visit_count": page_qs.count(),
+        "percent":((page_qs.count()*100.0)/qs.count()),
+        "total_visit_count": qs.count(),
+        
     }
     
+    path = request.path
+    print("path", path)
+    
     html_template = "home.html"
+    PageVisits.objects.create(path = request.path)
         
     return render(request,html_template,my_context)
 
