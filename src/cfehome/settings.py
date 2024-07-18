0548a5464f14ad5a1e34461af76a11cc227a4c6e
okeno1 <petercoin.1997@gmail.com>
2024-07-19 01:39:33 +0300

    Prepare for  railway -Production Environment

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -26,8 +26,14 @@ SECRET_KEY = 'django-insecure-cxv^&+84aotc1racp!@xh(evofo1=%-yc6@wjml5437m%dmqy6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    ".railway.app"           #https://saas.prod.railway.app
+]
+if DEBUG:
+    ALLOWED_HOSTS += [
+        "127.0.0.1",
+        "localhost"
+    ]
 
 # Application definition
 
--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -7,53 +7,42 @@ from visits.models import PageVisits
 
 this_dir = pathlib.Path(__file__).resolve().parent
 
-def home_page_view(request, *args, **kwargs):
-    
-    #query_set = PageVisits.objects.all()
-    page_qs = PageVisits.objects.filter(path=request.path)
-    qs = PageVisits.objects.all() 
-    my_title = "My page"
-    my_context={
-        "page_title": my_title,
-        "page_visit_count": page_qs.count(),
-        "percent":((page_qs.count()*100.0)/qs.count()),
-        "total_visit_count": qs.count(),
-        
-    }
-    
-    path = request.path
-    print("path", path)
-    
-    html_template = "home.html"
-    PageVisits.objects.create(path = request.path)
-        
-    return render(request,html_template,my_context)
+def home_view(request, *args, **kwargs):
+    return about_view(request, *args, **kwargs)
 
 
 def my_old_home_page_view(request, *args, **kwargs):
-    
-    my_title = "My page"
-    my_context={
-        "page_title":my_title
+    my_title = "My Page"
+    my_context = {
+        "page_title": my_title
     }
-    
-    
-    #print(this_dir)
-    html_ = f"""
+    html_ = """
     <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-</head>
+<html>
+
 <body>
-    <h1>{page_title} anything? </h1>
+    <h1>{page_title} anything?</h1>
 </body>
-</html>
-    """.format(**my_context) #page_title=my_title
-   # html_file_path = this_dir/"home.html"
-   # html_ = html_file_path.read_text()
-        
+</html>    
+""".format(**my_context) # page_title=my_title
+    # html_file_path = this_dir / "home.html"
+    # html_ = html_file_path.read_text()
     return HttpResponse(html_)
+
+def about_view(request, *args, **kwargs):
+    qs = PageVisits.objects.all()
+    page_qs = PageVisits.objects.filter(path=request.path)
+    try:
+        percent = (page_qs.count() * 100.0) / qs.count()
+    except:
+        percent = 0
+    my_title = "My Page"
+    html_template = "home.html"
+    my_context = {
+        "page_title": my_title,
+        "page_visit_count": page_qs.count(),
+        "percent": percent,
+        "total_visit_count": qs.count(),
+    }
+    PageVisits.objects.create(path=request.path)
+    return render(request, html_template, my_context)
