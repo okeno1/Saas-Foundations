5ab16aff4dd2f046266c34e86f719757b1009ae5
okeno1 <petercoin.1997@gmail.com>
2024-07-23 00:32:18 +0300

    static files, cdn styling with tailwindcss and flowbite

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -143,6 +143,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_BASE_DIR = BASE_DIR/"staticfiles"
+STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR/"vendors"
+
+#source(s) for python manage.py collectstatic
+STATICFILES_DIRS = [
+    STATICFILES_BASE_DIR
+]
+
+#Source(s) for python manage.py collectstatic
+#local cdn -> prod cdn
+STATIC_ROOT = BASE_DIR.parent / "local-cdn"
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
